5d524b5f62cec5682b3dafaf2dadbd008d315a37
yumaMK <75560508+yumaMK@users.noreply.github.com>
2020-12-06 18:22:35 +0900

    Add files via upload

---
+++ work.php
@@ -0,0 +1,30 @@
+<?php
+// $score = 15;
+// $line = 30;
+// switch ($score){
+//   case $score < $line:
+//     echo '赤点';
+//   break;
+//   case $score == 100:
+//     echo '満点';
+//   break;
+//   case $score > $line:
+//     echo '合格';
+//   break;
+// }
+
+// $amount = 1000;
+// $year = 0;
+// while($amount<=2000){
+//   $year++;
+//   $amount*=1.05;
+// }
+// echo $year.'年';
+
+$a = 1000;
+while($a >= 0){
+  echo $a;
+  $a -= 2;
+  echo '<br>';
+}
+?>
